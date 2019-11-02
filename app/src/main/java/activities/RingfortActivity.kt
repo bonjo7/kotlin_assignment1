f5083d158584e321b689151a37c1fff35623ebaa
Bernard Thompson <bethomps@redhat.com>
2019-11-02 20:44:53 +0000

    Created onclick for date

--- app/src/main/java/activities/RingfortActivity.kt
+++ app/src/main/java/activities/RingfortActivity.kt
@@ -1,11 +1,15 @@
 package activities
 
 import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.DatePicker
+import androidx.annotation.RequiresApi
 import com.example.bernardthompson_assignment1.R
 import com.example.bernardthompson_assignment1.RingfortMapsActivity
 import com.google.android.gms.maps.model.LatLng
@@ -21,6 +25,7 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.toast
+import java.util.*
 
 
 class RingfortActivity : AppCompatActivity(), AnkoLogger {
@@ -30,6 +35,7 @@ class RingfortActivity : AppCompatActivity(), AnkoLogger {
     val IMAGE_REQUEST = 1
     val LOCATION_REQUEST = 2
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ringfort)
@@ -41,6 +47,11 @@ class RingfortActivity : AppCompatActivity(), AnkoLogger {
         app = application as MainApp
 
         var edit = false
+
+        val calender = Calendar.getInstance()
+        val year = calender.get(Calendar.YEAR)
+        val month = calender.get(Calendar.MONTH)
+        val day = calender.get(Calendar.DAY_OF_MONTH)
 
         if (intent.hasExtra("ringfort_edit")) {
             edit = true
@@ -96,6 +107,16 @@ class RingfortActivity : AppCompatActivity(), AnkoLogger {
                 location.zoom = ringfort.zoom
             }
             startActivity (intentFor<RingfortMapsActivity>().putExtra("location", location))
+        }
+
+        visitedDateL.setOnClickListener {
+            val datepicker = DatePickerDialog(
+                this,
+                DatePickerDialog.OnDateSetListener { DatePicker, year, month, day ->
+                    visitedDateL.setText("$day - $month - $year")}, year, month, day
+                )
+            datepicker.show()
+
         }
 
 
