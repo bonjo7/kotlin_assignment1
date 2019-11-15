77820163be6d394be9e00c44cab98651b6d78fe6
Bernard Thompson <bethomps@redhat.com>
2019-11-15 21:12:35 +0000

    Displaying map

--- app/src/main/java/com/example/bernardthompson_assignment1/RingfortAllMapsActivity.kt
+++ app/src/main/java/com/example/bernardthompson_assignment1/RingfortAllMapsActivity.kt
@@ -3,20 +3,47 @@ package com.example.bernardthompson_assignment1
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
 
 import kotlinx.android.synthetic.main.activity_ringfort_all_maps.*
+import kotlinx.android.synthetic.main.content_ringfort_all_maps.*
 
 class RingfortAllMapsActivity : AppCompatActivity() {
+
+    lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ringfort_all_maps)
+        toolbar.title = title
         setSupportActionBar(toolbar)
+        mapView.onCreate(savedInstanceState);
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 
 }
