371854d7f4b6b92b277b770f46f3d5a798d31b14
Bernard Thompson <bethomps@redhat.com>
2019-11-15 21:33:36 +0000

    Ringforts added to map

--- app/src/main/java/com/example/bernardthompson_assignment1/RingfortAllMapsActivity.kt
+++ app/src/main/java/com/example/bernardthompson_assignment1/RingfortAllMapsActivity.kt
@@ -3,22 +3,52 @@ package com.example.bernardthompson_assignment1
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 import kotlinx.android.synthetic.main.activity_ringfort_all_maps.*
 import kotlinx.android.synthetic.main.content_ringfort_all_maps.*
+import main.MainApp
 
-class RingfortAllMapsActivity : AppCompatActivity() {
+class RingfortAllMapsActivity : AppCompatActivity(), GoogleMap.OnMarkerClickListener {
 
     lateinit var map: GoogleMap
 
+    lateinit var app: MainApp
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ringfort_all_maps)
-        toolbar.title = title
-        setSupportActionBar(toolbar)
-        mapView.onCreate(savedInstanceState);
 
+        app = application as MainApp
+
+        setContentView(R.layout.activity_ringfort_all_maps)
+        setSupportActionBar(toolbar)
+
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
+
+    }
+
+    fun configureMap() {
+        map.uiSettings.setZoomControlsEnabled(true)
+        app.ringforts.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.name).position(loc)
+            map.addMarker(options).tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+            map.setOnMarkerClickListener(this)
+        }
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        currentTitle.text = marker.title
+        return false
     }
 
     override fun onDestroy() {
@@ -45,5 +75,7 @@ class RingfortAllMapsActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
         mapView.onSaveInstanceState(outState)
     }
+
+
 
 }
